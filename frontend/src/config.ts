5b9a87186b5043015e2b85ee47e21d1dd85b6e71
Shivam Chhapola <shivam.chhapola.1920@gmail.com>
2024-10-11 10:55:42 +0530

    prod link added to config in frontend

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,2 @@
-export const BackendUrl = 'http://localhost:3000';
+// export const BackendUrl = "http://localhost:3000";
+export const BackendUrl = "https://teampumpkin-assignment-backend.vercel.app/";
