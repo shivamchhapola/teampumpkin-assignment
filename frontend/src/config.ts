baaea75fbf4af7155b6d9f176fdd8ea8d8a51e98
Shivam Chhapola <shivam.chhapola.1920@gmail.com>
2024-10-11 11:10:45 +0530

    added cors

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
 // export const BackendUrl = "http://localhost:3000";
-export const BackendUrl = "https://teampumpkin-assignment-backend.vercel.app/";
+export const BackendUrl = "https://teampumpkin-assignment-backend.vercel.app";
